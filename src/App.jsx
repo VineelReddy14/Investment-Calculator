ab6117bb7ab107b1f36565f49197e40beeaef038
Vineel Palla <vineelpalla14@gmail.com>
2025-01-02 15:27:58 -0800

    Outputting Content Conditionally(for duration greater than 0)

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,14 @@ function handleChange(inputIdentifier, newValue) {
   });
 }
 
+  const inputIsValid = userInput.duration >= 1;
+
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange}/>
-      <Results input={userInput}/>
+      {inputIsValid && <Results input={userInput}/>}
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
     </>
   );
 }
